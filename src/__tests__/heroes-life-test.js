3f18c34240726d6ca11aada595d0a5f7b503c9b8
Viktor <mr.viktor.tkachev@gmail.com>
2023-02-28 14:09:15 +0800

    added heroes-life.js file and test

---
+++ src/__tests__/heroes-life-test.js
@@ -0,0 +1,16 @@
+import heroesLifeUp from '../heroes-life';
+
+test('heroes life level comparison', () => {
+  const heroesList = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const result = heroesLifeUp(heroesList);
+  expect(result).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
---
+++ src/heroes-life.js
@@ -0,0 +1,3 @@
+const heroesLifeUp = (heroes) => heroes.sort((x, y) => y.health - x.health);
+
+export default heroesLifeUp;
